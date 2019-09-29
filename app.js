ffe5255cf772e11c138daf34325e089067b8d831
Adrian S MUresan <adriansmuresan@gmail.com>
2019-09-29 17:22:22 -0500

    Adds the book and UI constructors, adds an event listener on the book-form on submit

---
+++ app.js
@@ -0,0 +1,17 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {
+
+}
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit', function(e) {
+ 
+  e.preventDefault();
+} );
