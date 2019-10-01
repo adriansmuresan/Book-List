af8ad2bd2707dd1845c0352ef148bb4d4b9efc45
Adrian S MUresan <adriansmuresan@gmail.com>
2019-10-01 14:33:20 -0500

    Adds the code from es5 approach to the methods for the UI class

--- appes6.js
+++ appes6.js
@@ -7,19 +7,53 @@ class Book {
 }
 
 class UI {
-  addBookToList() {
+  addBookToList(book) {
+    const list = document.getElementById('book-list');
 
+    // Create tr element
+    const row = document.createElement('tr');
+
+    // Insert columns
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class="delete">X</td>
+    `;
+
+    list.appendChild(row);
   }
 
-  showAlert() {
+  showAlert(message, className) {
+    // Create div
+  const div = document.createElement('div');
+  // Add classes 
+  div.className = `alert ${className}`;
+  // Add text
+  div.appendChild(document.createTextNode(message));
+  // Get parrent
+  const container = document.querySelector('.container');
+  // Get form
+  const form = document.querySelector('#book-form');
+  // Insert alert
+  container.insertBefore(div, form);
 
+  // Timeout after 3 sec
+  setTimeout(function() {
+    document.querySelector('.alert').remove();
+  }, 3000);
   }
 
-  deleteBook() {
-
+  deleteBook(target) {
+    if(target.className === 'delete'){
+      target.parentElement.parentElement.remove();
+    }
   }
 
   clearFields() {
-    
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+
   }
 }
